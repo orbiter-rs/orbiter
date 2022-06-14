8f488aea711f26099dee5d03ea0b3c219c0f4f60
Chris Chou <chhschou@gmail.com>
2022-06-14 21:57:53 +1200

    fix: panic when src file not found

--- src/hooks/src.rs
+++ src/hooks/src.rs
@@ -2,10 +2,10 @@ use std::fs;
 
 pub fn src(files: &Vec<String>) -> Result<(), Box<dyn std::error::Error>> {
     for f in files {
-        let f_path = fs::canonicalize(&f).unwrap().display().to_string();
-        let cmd = format!(". {}", &f_path);
-
-        println!("{}", &cmd);
+        if let Ok(f_path) = fs::canonicalize(&f) {
+            let cmd = format!(". {}", &f_path.display().to_string());
+            println!("{}", &cmd);
+        };
     }
 
     Ok(())
